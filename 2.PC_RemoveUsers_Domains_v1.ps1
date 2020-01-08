d4cccf60bfeffba78dd7c95348bcfb1d4961469f
FGRoy <fgonzalezroy@gmail.com>
2020-01-08 18:53:54 +0100

    Updated v1.1
    Filtered non existing users
    Format improved

--- 2.PC_RemoveUsers_Domains_v1.ps1
+++ 2.PC_RemoveUsers_Domains_v1.ps1
@@ -1,11 +1,11 @@
-﻿## CONNECTING TO PARNER CENTER:
+## CONNECTING TO PARNER CENTER:
 Import-Module MSOnline
 
 ## Write-Host "Details for Partner Center Credentials (same password and login needed for all accounts `
 ## if MFA enabled please allow some time for the script to complete)..."
 ## * # Remove next three lines comments plus $O365Cred below for user and password prompt for every PC Account # * # Ad1rectory.
 #$SecUser = Read-Host -Prompt "Enter your partner center account username (before the @) "
-#$SecPass = Read-Host -AsSecureString -Prompt "Enter your partner center account password "
+#$SecPass = Read-Host -Prompt "Enter your partner center account password "
 #$SecPass = ConvertTo-SecureString $SecPass -AsPlainText -Force 
 
 #Path for csv formated list of users and Partner Center domains. RELATIVE PATH to the dir script is running 
@@ -37,10 +37,13 @@ Import-Csv -Path $PCDomains_Csv |
         Import-Csv -Path $UserstoDelete_Csv |
             foreach {
                 $UPN = $_.Firstname + "." + $_.Lastname + $Maildomain
-                Get-MsolUser -UserPrincipalName $UPN | Remove-MsolUser -Force
+                $UserExists = Get-MsolUser -UserPrincipalName $UPN -ErrorAction SilentlyContinue
+
+                If (!$UserExists)  { Write-Host "User $UPN doesn't exist at:" $Maildomain "`n"
+                } Else { Get-MsolUser -UserPrincipalName $UPN | Remove-MsolUser -Force }
                 }
 
-    Write-Host "The following users are now on the recycle bin: `n #To delete all users from Recycle: `n Get-MsolUser -ReturnDeletedUsers | Remove-MsolUser -RemoveFromRecycleBin -Force"
+    Write-Host "The following users are now on the recycle bin: `n #To delete all users from Recycle: `n Get-MsolUser -ReturnDeletedUsers | Remove-MsolUser -RemoveFromRecycleBin -Force `n"
     # List deleted users on recycle bin.
     Get-MsolUser -ReturnDeletedUsers
  }
